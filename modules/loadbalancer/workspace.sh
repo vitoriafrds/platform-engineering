be5386890dc5967ebafbd2913fe81ab627880599
Júlio Oliveira <julio.oliveira@philips.com>
2024-08-13 20:45:24 -0300

    fix: leo changes

--- modules/loadbalancer/workspace.sh
+++ modules/loadbalancer/workspace.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-
-#6. Crie um ambiente de dev e um de prod
-terraform workspace new dev || echo "Workspace 'dev' already exists"
-terraform workspace new prod || echo "Workspace 'prod' already exists"
-
+ 
 apply_workspace() {
     local workspace=$1
     terraform workspace select $workspace
+    terraform init
     terraform apply -auto-approve
+    wait $!
 }
-
+ 
+#6. Crie um ambiente de dev e um de prod
+terraform workspace new dev || echo "Workspace 'dev' already exists"
+ 
 apply_workspace dev
+ 
+ 
+terraform workspace new prod || echo "Workspace 'prod' already exists"
+ 
 apply_workspace prod
-
